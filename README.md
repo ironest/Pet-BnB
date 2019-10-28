e1a53229ed140d5e8050637e492da61d3d1e7a86
ironest <ironest@gmail.com>
2019-10-29 01:24:15 +1100

    Fixed typos in model association

--- README.md
+++ README.md
@@ -3,11 +3,13 @@
 ## Project Timeline
 
 * **Day 1 (Friday 25-Oct-2019)**
+
 Project idea approved
 Wireframes sketched out
 ERD diagram designed
 
 * **Day 2 (Monday 28-Oct-2019)** 
+
 Created Models
 Create Tables
 Amended DB with Migrations
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,7 +1,9 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :petsitter
-  belongs_to :pet
-  has_one :transactions
+  has_many :bookings_pets
+  has_many :pets, through: :bookings_pets
+  # has_one :transaction
+  has_one :owner, foreign_key: "booking_id", class_name: "Transaction"
   enum status: { pending: 0, accepted: 1, rejected: 2, payed: 3 }
 end
