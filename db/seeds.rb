9288cbfc5d70afdd43a421074f167a791cb3190b
ironest <ironest@gmail.com>
2019-10-31 14:26:48 +1100

    Attaching images to Seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -24,6 +24,11 @@ for i in 1..30
 
 end
 
+# Just for my conviniency 
+u = User.first
+u.first_name = "Erin"
+u.email = "erin@brown.net"
+u.save
 
 # Out of the previous 30 USERS, 15 of them are randomly converted into PETSITTERS
 random_petsitter_ids = User.pluck(:id).sample(15)
@@ -69,15 +74,35 @@ pet_types = {
     "rabbit":     6
 }
 
-
 for user_id in random_petowner_ids
 
     # Creating a pet for each PETOWNER
-    p = Pet.create(
+
+    pet_type = rand(0..6)
+    # Preparing the proper image keywords for LoremFlickr
+    if pet_type < 5
+        img_key = "dog"
+    elsif pet_type == 5
+        img_key = "cat"
+    else
+        img_key = "bunny"
+    end
+
+    pet = Pet.create(
         name: Faker::Creature::Cat.name,
-        pet_type: pet_types[pet_types.keys.sample],
+        pet_type: pet_types[pet_types.keys[pet_type]],
         user_id: user_id
     )
+
+    temp_picture = Down.download(
+        Faker::LoremFlickr.image(
+            size: "400x400",
+            search_terms: ['pet',img_key],
+            match_all: true
+            ) + "?random=#{i}"
+        )
+
+    pet.picture.attach(io: temp_picture, filename: File.basename(temp_picture.path))
 
     # Creating a booking for each PETOWNER
     b = Booking.create(
@@ -88,7 +113,7 @@ for user_id in random_petowner_ids
         status: 1 # accepted
     )
 
-    p.bookings = [b]
+    pet.bookings = [b]
 
 end
 
