71907337b1f1cc5d0cd080c66c1d2165d269a065
ironest <ironest@gmail.com>
2019-11-02 17:18:26 +1100

    Delete on cascade (Bookings) when deleting a Petsitter

--- app/models/petsitter.rb
+++ app/models/petsitter.rb
@@ -1,7 +1,7 @@
 class Petsitter < ApplicationRecord
   belongs_to :user
-  has_many :bookings
+  has_many :bookings, dependent: :destroy
   has_many :petsitters_services
-  has_many :services, through: :petsitters_services
+  has_many :services, through: :petsitters_services, dependent: :destroy
   enum status: { active: 0, inactive: 1 }
 end
