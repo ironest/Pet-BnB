f1e6da5d62d4d819265678e9274d8b279f33b08c
ironest <ironest@gmail.com>
2019-11-02 17:21:54 +1100

    Delete on cascade (Bookings_Pets) when deleting a Booking

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,7 +1,7 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :petsitter
-  has_many :bookings_pets
+  has_many :bookings_pets, dependent: :destroy
   has_many :pets, through: :bookings_pets
   has_one :payment
   enum status: { pending: 0, accepted: 1, rejected: 2, payed: 3 }
