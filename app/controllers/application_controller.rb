72c4d1d078f2f598613b7498613080540154f0ec
ironest <ironest@gmail.com>
2019-11-07 11:40:21 +1100

    Allowing users to load their own profile picture

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
 
     def configure_permitted_parameters
         devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :dob])
-        devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :dob]) 
+        devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :dob, :picture]) 
     end
 
 end
